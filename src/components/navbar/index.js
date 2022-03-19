1a1f7485b16edb7f6b5372a6693c6365cb227614
fadilaharifki <fadilah.arifki29@gmail.com>
2022-03-19 23:12:09 +0700

    exp

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -4,29 +4,29 @@ export default function NavBarMenu() {
     return (
         <nav id="home" className="fixed-top navbar navbar-expand-lg navbar-light bg-success">
             <div className="container-fluid">
-                <a className="navbar-brand text-white" href="#home">Export Palm Indonesia</a>
+                <a className="navbar-brand text-white" href="/#home">Export Palm Indonesia</a>
                 <button className="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon text-white" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link text-white active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link text-white active" aria-current="page" href="/#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-white" href="#about">About</a>
+                            <a className="nav-link text-white" href="/#about">About</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-white" href="#feature">Feature</a>
+                            <a className="nav-link text-white" href="/#feature">Feature</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-white" href="#product">Product</a>
+                            <a className="nav-link text-white" href="/#product">Product</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-white" href="#teams">Team</a>
+                            <a className="nav-link text-white" href="/#teams">Team</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-white" href="#contact">Contact</a>
+                            <a className="nav-link text-white" href="/#contact">Contact</a>
                         </li>
                     </ul>
                 </div>
